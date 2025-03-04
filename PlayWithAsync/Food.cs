1e6d2d0dec12693253f5dc47d716910a3e70e734
Thiago Prohaska <thiago.prohaska@gmail.com>
2025-03-04 20:10:26 +0100

    create project

---
+++ PlayWithAsync/Food.cs
@@ -0,0 +1,28 @@
+﻿namespace PlayWithAsync;
+public abstract class Food
+{
+    private readonly TimeSpan cookTime;
+
+    public string Name { get; set; }
+    protected Food(TimeSpan cookTime)
+    {
+        this.cookTime = cookTime;
+        Name = GetType().Name;
+    }
+
+    public async Task Cook()
+    {
+
+        Console.WriteLine($"Cooking {Name}");
+
+        await Task.Delay(cookTime);
+
+        Console.WriteLine($"{Name} Completed");
+
+    }
+}
+
+public class Turkey(): Food(TimeSpan.FromSeconds(5));
+public class MashedPotatoes() : Food(TimeSpan.FromSeconds(2));
+public class Gravy() : Food(TimeSpan.FromSeconds(1));
+public class Stuffing() : Food(TimeSpan.FromSeconds(2));
---
+++ PlayWithAsync/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using PlayWithAsync;
+
+Console.WriteLine("Cooking Started");
+
+
+var turkey = new Turkey();
+var gravy = new Gravy();
+
+await Task.WhenAll(turkey.Cook(), gravy.Cook());
+
+Console.WriteLine("Ready to eat");
